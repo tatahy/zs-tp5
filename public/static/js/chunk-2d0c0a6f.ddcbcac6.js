7038afa6f5826e60b078cfc4960e1d4375ca8287
tatahy <hy135@139.com>
2020-06-21 02:29:35 +0800

    frontend updated

--- public/static/js/chunk-2d0c0a6f.ddcbcac6.js
+++ public/static/js/chunk-2d0c0a6f.ddcbcac6.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c0a6f"],{4360:function(e,t,a){"use strict";a.r(t);var n=a("2b0e"),s=a("2f62"),i={showPage:{index:!0,admin:!1}},o={setShowPage:function(e){var t=arguments.length>1&&void 0!==arguments[1]?arguments[1]:"index",a=e.showPage;for(var n in a)a[n]=n===t}},d={},u={},r={},c={};n["default"].use(s["a"]);var w=new s["a"].Store({strict:!1,state:d,getters:u,mutations:r,actions:c}),f=w,m={adminModules:["index","login"]},g={},l={},h={},p={namespaced:!0,state:m,getters:g,actions:h,mutations:l};n["default"].use(s["a"]);var v=new s["a"].Store({modules:{index:f,admin:p},strict:!1,state:i,mutations:o});t["default"]=v}}]);
-//# sourceMappingURL=chunk-2d0c0a6f.096cb1ee.js.map
+//# sourceMappingURL=chunk-2d0c0a6f.ddcbcac6.js.map
